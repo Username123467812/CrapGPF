c0b08c0978dfd04604570e80c463e53f1a9a773c
Username123467812 <erbrasel@madison.k12.wi.us>
2025-04-03 17:47:39 +0000

    cool data set stuff

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CrapGPF
-CrapGPF is a Generally Pathetic Failure (GPF) ai
+CrapGPF is a Generally Pathetic Failure (GPF) sorta-AI. (Disclaimer: Not really generatiuve ai)
---
+++ ai-engine.py
@@ -0,0 +1,5 @@
+p_array = ["What's your favorite color?","How tall am I?","How do I start a war?","What color is a banana?","How to escape a country?"]
+r_array = ["I like the color orange.","Let's try 1.7 meters.","Please don't.","A banana is yellow.","Try crossing a border."]
+
+def prompt(text):
+    text_array = text.split()
